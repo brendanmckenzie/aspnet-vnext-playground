6fa032f6e0905dfc26e92cf8b99c5a79a92a1986
Brendan McKenzie <hello@brendanmckenzie.com>
2014-06-29 20:57:49 +1000

    updated to include client listing.

--- src/App.Web/Hubs/ChatHub.cs
+++ src/App.Web/Hubs/ChatHub.cs
@@ -1,13 +1,52 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
 
 namespace App.Web.Hubs
 {
 	public class Chat : Hub
 	{
+		static Dictionary<string, Client> _clients = new Dictionary<string, Client>();
+
+		public override Task OnConnected()
+		{
+			var client = new Client { 
+				ConnectionId = Context.ConnectionId, 
+				Name = "Guest", 
+				Connected = DateTime.Now };
+
+			_clients.Add(Context.ConnectionId, client);
+
+			Clients.All.client_connected(client);
+
+			return base.OnConnected();
+		}
+
+		public override Task OnDisconnected()
+		{
+			Clients.All.client_disconnected(Context.ConnectionId);
+
+			_clients.Remove(Context.ConnectionId);
+
+			return base.OnDisconnected();
+		}
+
 		public void Send(string name, string message)
 		{
 			Clients.All.send(new Message { Sent = DateTime.Now, Sender = name, Content = message });
+		}
+
+		public void SetName(string name)
+		{
+			Clients.All.set_name(Context.ConnectionId, name);
+			
+			_clients[Context.ConnectionId].Name = name;
+		}
+
+		public IEnumerable<Client> List()
+		{
+			return _clients.Values;
 		}
 	}
 
@@ -16,5 +55,12 @@ namespace App.Web.Hubs
 		public string Sender { get; set; }
 		public DateTime Sent { get; set; }
 		public string Content { get; set; }
+	}
+
+	public class Client
+	{
+		public string ConnectionId { get; set; }
+		public string Name { get; set; }
+		public DateTime Connected { get; set; }
 	}
 }
--- src/App.Web/content/site/js/app/modules/chat/chat.js
+++ src/App.Web/content/site/js/app/modules/chat/chat.js
@@ -1,29 +1,66 @@
 angular.module('App.Modules').controller('ChatCtrl', ['$scope', '$rootScope', 'Hub', 'PageService', function ($scope, $rootScope, Hub, PageService) {
     PageService.setTitle('Chat');
     $scope.messages = [];
-    $scope.name = localStorage['name'];
+    $scope.clients = [];
     
     $scope.hub = new Hub('chat', {
         'send': function (message) {
             $scope.messages.push(message);
             $scope.$$phase || $scope.$apply();
+        },
+        'client_connected': function (client) {
+            $scope.clients.push(client);
+            $scope.$$phase || $scope.$apply();
+        },
+        'client_disconnected': function (id) {
+            for (var i = 0; i < $scope.clients.length; i++) {
+                if ($scope.clients[i].ConnectionId == id) {
+                    $scope.clients.splice(i, 1);
+                    break;
+                }
+            }
+            $scope.$$phase || $scope.$apply();
+        },
+        'set_name': function (id, name) {
+            for (var i = 0; i < $scope.clients.length; i++) {
+                if ($scope.clients[i].ConnectionId == id) {
+                    $scope.clients[i].Name = name;
+                    break;
+                }
+            }
+            $scope.$$phase || $scope.$apply();
         }
     },
-    ['send'], $scope);
+    ['Send', 'SetName', 'List'], $scope);
 
 
-    $scope.hub.start().then(function() { 
+    $scope.hub.start().then(function() {
+        $scope.hub.List().then(function (data) {
+            $scope.clients = data;
+            $scope.$$phase || $scope.$apply();
+        });
+
+        if (localStorage['name']) {
+            $scope.name = localStorage['name'];
+            $scope.hub.SetName($scope.name);
+        }
+        else {
+            $scope.name = 'Guest';
+        }
+        
         $scope.ready = true;
         $scope.$$phase || $scope.$apply();
     });
 
     $scope.send = function () {
-        $scope.hub.send($scope.name, $scope.message);
+        $scope.hub.Send($scope.name, $scope.message);
 
         $scope.message = '';
     }
 
     $scope.storeName = function () {
         localStorage['name'] = $scope.name;
+
+        $scope.hub.SetName($scope.name);
     };
 }]);
